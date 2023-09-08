25d4f3fbce76f0ce39c584f6672bb6f5eb3b9535
vishalghadiya <vishalg@zignuts.com>
2023-09-08 18:26:43 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+// import { UserButton } from '@clerk/nextjs'
+// import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      {/* <UserButton afterSignOutUrl='/'/> */}
+    <h1 className="head-text text-left">home </h1>
+    </div>
+  )
+}
